4d3e0678246cdc2236d22b05ae39415d96af8873
mikhail <grin1946>
2015-01-06 11:29:11 +0200

    added Set<?> to Json array transformer

---
+++ src/com/mikerusoft/jsonable/transform/SetTransformer.java
@@ -0,0 +1,36 @@
+package com.mikerusoft.jsonable.transform;
+
+import com.mikerusoft.jsonable.utils.Outputter;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+/**
+ * Transforms Set into Json array
+ */
+public class SetTransformer implements Transformer {
+    @Override
+    public boolean match(Object o) {
+            return Set.class.isAssignableFrom(o.getClass());
+        }
+
+    @Override
+    public void transform(Object o, Outputter<String> out, String... groups) throws IOException, IllegalAccessException, InvocationTargetException {
+        Set<?> l = (Set<?>)o;
+        out.write("[");
+        int i = 0;
+        for (Object p : l) {
+            TransformerFactory.get(p).transform(p, out, groups);
+            if (i != l.size() - 1)
+                out.write(",");
+            i++;
+        }
+        out.write("]");
+    }
+
+    @Override
+    public int matchPriority() {
+        return Transformer.HIGH_PRIORITY;
+    }
+}
